11b6c84ee8ec0a9fedc4559555b3083b5941a032
Divyansh Jha <divyanshj.16@gmail.com>
2019-04-08 00:08:27 +0530

    update the notebook in architecture fixes and some comments in spadedata.py

--- spadedata.py
+++ spadedata.py
@@ -6,6 +6,36 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 from torch import nn
 
+# class ImageImageSegment(ItemBase):
+#     def __init__(self, img1, segment):
+#         self.img1, self.segment = img1, segment
+#         self.obj, self.data = (img1, segment), [-1+2*img1.data, segment.data]
+
+#     def apply_tfms(self, tfms, **kwargs):
+#         self.img1 = self.img1.apply_tfms(tfms, **kwargs)
+#         self.segment = self.segment.apply_tfms(tfms, **kwargs)
+#         self.data = [-1+2*self.img1.data, self.segment.data]
+#         return self
+
+#     def to_one(self): return Image(0.5+torch.cat(self.data,2)/2)
+
+
+# class SpadeLabelList(SegmentationItemList):
+#     "`ItemList` for segmentation masks."
+# #     _processor = SpadeProcessor
+#     def __init__(self, items, **kwargs):
+#         super().__init__(items, **kwargs)
+#              #  loss function to be added
+
+#     def open(self, fn, **kwargs):
+#         return open_image(fn[0], **kwargs), open_mask(fn[1], **kwargs)
+    
+#     def analyze_pred(self, pred, thresh:float=0.5):
+#         return pred
+    
+#     def reconstruct(self, t):
+#         return Image(t[0]), ImageSegment(t[1])
+
 class SpadeLabelList(SegmentationItemList):
     "`ItemList` for segmentation masks."
 #     _processor = SpadeProcessor
